e59d3df66ca711d8abe02c636bf0fb644c543f3c
Kim <lumierebuds@gmail.com>
2024-09-19 16:20:49 +0900

    feat:템플릿 설정& 레이아웃

---
+++ src/main/java/com/lumierebuds/boards/BoardAppApplication.java
@@ -0,0 +1,12 @@
+package com.lumierebuds.boards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lumierebuds/boards/BoardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumierebuds.boards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
